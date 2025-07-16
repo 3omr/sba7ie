bb9dfe9201cd134d4f638110b994714b064481f7
3omr <oabdelaleem3@gmail.com>
2025-07-16 17:00:07 +0300

    feat: Register StudentRepo in get_it and refactor StudentRepo to use dependency injection

--- lib/core/get_it/get_it.dart
+++ lib/core/get_it/get_it.dart
@@ -1,4 +1,5 @@
 import 'package:get_it/get_it.dart';
+import 'package:tasneem_sba7ie/feature/students/data/repos/student_repo.dart';
 import 'package:tasneem_sba7ie/feature/teachers/data/repos/teacher_management_repo.dart';
 import 'package:tasneem_sba7ie/feature/teachers/data/repos/teacher_repo.dart';
 import 'package:tasneem_sba7ie/feature/teachers/logic/teacher_cubit/teacher_cubit.dart';
@@ -10,19 +11,21 @@ void getItSetup() {
   // DB
   getIt.registerLazySingleton<Db>(() => Db());
 
-  // Registering the TeacherRepo as a singleton
+  // Teacher
   getIt.registerLazySingleton<TeacherRepo>(() => TeacherRepo(getIt<Db>()));
+
   getIt.registerFactory<TeacherCubit>(
     () => TeacherCubit(getIt<TeacherRepo>()),
   );
 
-  // Registering the TeacherManagementRepo as a singleton
   getIt.registerLazySingleton<TeacherManagementRepo>(
     () => TeacherManagementRepo(getIt<Db>()),
   );
 
-  // Registering the TeacherManagementCubit as a singleton
   getIt.registerFactory<TeacherManagementCubit>(
     () => TeacherManagementCubit(getIt<TeacherManagementRepo>()),
   );
+
+  // student
+  getIt.registerLazySingleton<StudentRepo>(() => StudentRepo(getIt<Db>()));
 }
--- lib/feature/students/data/repos/student_repo.dart
+++ lib/feature/students/data/repos/student_repo.dart
@@ -1,31 +1,32 @@
 import 'package:tasneem_sba7ie/feature/students/data/models/student_model.dart';
 import 'package:tasneem_sba7ie/core/database/tabels_name.dart';
-
-import '../../../../core/database/db.dart';
+import 'package:tasneem_sba7ie/core/database/db.dart';
 
 class StudentRepo {
-  Db db = Db();
+  final Db _db;
+
+  StudentRepo(this._db);
 
   Future<List<Student>> getStudents() async {
     List<Student> studentsList = [];
-    List<Map<dynamic, dynamic>> resp = await db.readData(studentsTable);
+    List<Map<dynamic, dynamic>> resp = await _db.readData(studentsTable);
     studentsList.addAll(resp.map((e) => Student.fromJson(e)));
     return studentsList;
   }
 
   Future<int> addStudent(Map<String, Object?> student) async {
-    int resp = await db.insertData(studentsTable, student);
+    int resp = await _db.insertData(studentsTable, student);
     return resp;
   }
 
   updateStudent(Student oldStudentData, Student newStudentData) async {
-    int resp = await db.updateData(
+    int resp = await _db.updateData(
         studentsTable, newStudentData.toJson(), "id = ${oldStudentData.id}");
     return resp;
   }
 
   deleteStudent(int studentId) async {
-    int resp = await db.deleteData(studentsTable, "id = $studentId");
+    int resp = await _db.deleteData(studentsTable, "id = $studentId");
     return resp;
   }
 }
